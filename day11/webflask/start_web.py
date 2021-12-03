59cfa07f5784231f680bafdf8492551534d8b898
Parkjj94 <ind01158@naver.com>
2021-12-03 12:49:17 +0900

    day12

--- day11/webflask/start_web.py
+++ day11/webflask/start_web.py
@@ -19,10 +19,29 @@ def register():
     else:
         return render_template('register.html')  # GET 방식
 
-@app.route("/loop_index")
+@app.route("/loop_index/")
 def get_loopindex():
     items = ['a', 'b', 'c', 'd']
     return render_template('loop_index.html', items=items)
+
+@app.route("/even_odd/", methods = ["GET", "POST"])
+def even_odd():
+    if request.method == "POST":
+        try:
+        # 데이터 수집
+            num = int(request.form['num'])  # 문자를 숫자로 바꿈
+        except ValueError:
+            error_message = "숫자를 입력해주세요"
+            return render_template('even_odd.html', error_message=error_message)
+        else:
+        # 데이터 처리
+            if num % 2 == 0:
+                result = "짝수입니다."
+            else:
+                result = "홀수입니다."
+            return render_template('calc_result.html', num=num, result=result)
+    else:   # GET 인 경우
+        return render_template('even_odd.html')
 
 app.run(debug=True)     # 서비스하기전 개발 모드
 
